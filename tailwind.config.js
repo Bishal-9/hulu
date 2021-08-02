b7f446538b41c2f2589d8eb273e0aec0e4867f1a
Bishal-9 <goonbishal.bg@gmail.com>
2021-08-02 23:03:54 +0530

    Full Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['hover', 'focus'],
+    },
   },
-  plugins: [],
+  corePlugins: {
+    animation: true,
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
 }
